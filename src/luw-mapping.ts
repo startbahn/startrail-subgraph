1101b7c81404f284d5670767cd6bca2cb006317e
Chris Hatch <hatch@tuta.io>
2020-08-27 08:37:17 +0700

    update subgraph to 1.0.0

--- src/luw-mapping.ts
+++ src/luw-mapping.ts
@@ -1,5 +1,5 @@
 
-import { Bytes } from '@graphprotocol/graph-ts'
+import { Bytes, log } from '@graphprotocol/graph-ts'
 
 import { CreateLicensedUserWallet as CreateLUWEvent } from '../generated/LicensedUserEvent/LicensedUserEvent'
 import { LicensedUserWallet } from '../generated/schema'
@@ -13,10 +13,21 @@ export function handleCreateLicensedUserWallet(event: CreateLUWEvent): void {
   luw.requiredConfirmation = event.params.requiredConfirmation.toI32()
   luw.englishName = event.params.englishName
   luw.originalName = event.params.originalName
-  luw.userType = event.params.userType
   luw.owners = event.params.owners as Array<Bytes>
 
+  let userType = event.params.userType
+  switch(userType) {
+    case 0:
+      luw.userType = 'admin'
+    case 1:
+      luw.userType = 'handler'
+    case 2:
+      luw.userType = 'artist'
+    default:
+      log.error(`unhandled userType: {}`, [userType.toString()])
+  }
+
   luw.createdAt = luw.updatedAt = eventUTCMillis(event)
-  
+ 
   luw.save()
 }
