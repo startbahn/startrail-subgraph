49093dea8eda1f317579a7d363cdb9cc1e789158
Chris Hatch <hatch@tuta.io>
2021-01-21 22:48:47 +0700

    API-1026 handle ALL LicensedUserManager events new entities MetaTxRequestType, MetaTxExecution

--- src/luw-mapping.ts
+++ src/luw-mapping.ts
@@ -1,17 +1,26 @@
-import { BigInt, Bytes, log } from '@graphprotocol/graph-ts'
+import { BigInt, Bytes, log, store } from '@graphprotocol/graph-ts'
 
 import { CreateLicensedUserWallet as LegacyCreateLUWEvent } from '../generated/LicensedUserEvent/LicensedUserEvent'
 import {
-  CreateLicensedUserWalletMulti as CreateLUWMultiEvent,
-  CreateLicensedUserWalletSingle as CreateLUWSingleEvent,
+  AddedOwner as AddedOwnerEvent,
+  ChangedThreshold as ChangedThresholdEvent,
+  CreateLicensedUserWallet as CreateLUWEvent,
+  ExecutionSuccess as ExecutionSuccessEvent,
+  RemovedOwner as RemovedOwnerEvent,
+  RequestTypeRegistered as RequestTypeRegisteredEvent,
+  RequestTypeUnregistered as RequestTypeUnregisteredEvent,
   UpdateEnglishName as UpdateEnglishNameEvent,
   UpdateOriginalName as UpdateOriginalNameEvent,
   UpgradeLicensedUserWalletToMulti as UpgradeLicensedUserWalletToMultiEvent,
 } from '../generated/LicensedUserManager/LicensedUserManager'
-import { LicensedUserWallet } from '../generated/schema'
+import {
+  LicensedUserWallet,
+  MetaTxExecution,
+  MetaTxRequestType,
+} from '../generated/schema'
 import { eventUTCMillis } from './utils'
 
-export function userType(n: i32): string {
+function userType(n: i32): string {
   switch (n) {
     case 0:
       return "handler";
@@ -23,7 +32,7 @@ export function userType(n: i32): string {
   }
 }
 
-export function legacyUserType(n: i32): string {
+function legacyUserType(n: i32): string {
   switch (n) {
     case 0:
       return "admin";
@@ -37,9 +46,7 @@ export function legacyUserType(n: i32): string {
   }
 }
 
-export function handleCreateLicensedUserWalletSingle(
-  event: CreateLUWSingleEvent
-): void {
+export function handleCreateLicensedUserWallet(event: CreateLUWEvent): void {
   let timestampMillis = eventUTCMillis(event);
   let luwId = event.params.walletAddress.toHexString();
   let luw = LicensedUserWallet.load(luwId);
@@ -47,35 +54,7 @@ export function handleCreateLicensedUserWalletSingle(
   // New wallet (not a migration from legacy)
   // If it's a migration we skip setting these values as they are already set
   if (luw == null) {
-    log.info("creating new single LUW {}", [luwId]);
-    luw = new LicensedUserWallet(luwId);
-    luw.walletAddress = event.params.walletAddress;
-    luw.englishName = event.params.englishName;
-    luw.originalName = event.params.originalName;
-    luw.owners = [event.params.owner];
-    luw.threshold = BigInt.fromI32(1);
-    luw.createdAt = timestampMillis;
-  } else {
-    log.info("migrating legacy single LUW {}", [luwId]);
-  }
-
-  luw.userType = userType(event.params.userType);
-  luw.updatedAt = timestampMillis;
-
-  luw.save();
-}
-
-export function handleCreateLicensedUserWalletMulti(
-  event: CreateLUWMultiEvent
-): void {
-  let timestampMillis = eventUTCMillis(event);
-  let luwId = event.params.walletAddress.toHexString();
-  let luw = LicensedUserWallet.load(luwId);
-
-  // New wallet (not a migration from legacy)
-  // If it's a migration we skip setting these values as they are already set
-  if (luw == null) {
-    log.info("creating new multi LUW {}", [luwId]);
+    log.info("creating new LUW [{}, {}]", [event.params.originalName, luwId]);
     luw = new LicensedUserWallet(luwId);
     luw.walletAddress = event.params.walletAddress;
     luw.englishName = event.params.englishName;
@@ -84,12 +63,82 @@ export function handleCreateLicensedUserWalletMulti(
     luw.threshold = event.params.threshold;
     luw.createdAt = timestampMillis;
   } else {
-    log.info("migrating legacy multi LUW {}", [luwId]);
+    log.info("migrating legacy LUW {}", [luwId]);
   }
 
   luw.userType = userType(event.params.userType);
   luw.updatedAt = timestampMillis;
 
+  luw.save();
+}
+
+export function handleAddedOwner(event: AddedOwnerEvent): void {
+  let luwId = event.params.wallet.toHexString();
+  let luw = LicensedUserWallet.load(luwId);
+
+  if (luw == null) {
+    log.error("received AddedOwner event for unknown LUW: {}", [luwId]);
+    return;
+  }
+
+  let owner = event.params.owner;
+  log.info("adding owner [{}] to LUW [{}]", [owner.toHexString(), luwId]);
+
+  // this 3 step assign, push, reassign is necessary here:
+  // (see https://thegraph.com/docs/assemblyscript-api#api-reference):
+  let owners = luw.owners;
+  owners.push(owner);
+  luw.owners = owners;
+
+  luw.updatedAt = eventUTCMillis(event);
+  luw.save();
+}
+
+export function handleRemovedOwner(event: RemovedOwnerEvent): void {
+  let luwId = event.params.wallet.toHexString();
+  let luw = LicensedUserWallet.load(luwId);
+
+  if (luw == null) {
+    log.error("received RemovedOwner event for unknown LUW: {}", [luwId]);
+    return;
+  }
+
+  let owner = event.params.owner;
+  let ownerIdx = luw.owners.indexOf(owner);
+  if (ownerIdx == -1) {
+    log.error("owner in RemovedOwner [{}] is not in the indexed owner list", [
+      owner.toHexString(),
+    ]);
+    return;
+  }
+
+  log.info("removing owner [{}] to LUW [{}]", [owner.toHexString(), luwId]);
+
+  // assign to separate variable is required when updating array, see here:
+  // (see https://thegraph.com/docs/assemblyscript-api#api-reference):
+
+  // TODO: does this work? or need to assign to a separate array first ...?
+  luw.owners = luw.owners.splice(ownerIdx, 1);
+  luw.updatedAt = eventUTCMillis(event);
+  luw.save();
+}
+
+export function handleChangedThreshold(event: ChangedThresholdEvent): void {
+  let luwId = event.params.wallet.toHexString();
+  let luw = LicensedUserWallet.load(luwId);
+
+  if (luw == null) {
+    log.error("received ChangedThreshold event for unknown LUW: {}", [luwId]);
+    return;
+  }
+
+  let threshold = BigInt.fromI32(event.params.threshold);
+  log.info("changing threshold to [{}] for LUW [{}]", [
+    threshold.toString(),
+    luwId,
+  ]);
+  luw.threshold = threshold;
+  luw.updatedAt = eventUTCMillis(event);
   luw.save();
 }
 
@@ -140,6 +189,55 @@ export function handleUpdateOriginalName(event: UpdateOriginalNameEvent): void {
   luw.updatedAt = eventUTCMillis(event);
   luw.save();
 }
+
+export function handleRequestTypeRegistered(
+  event: RequestTypeRegisteredEvent
+): void {
+  let typeHash = event.params.typeHash;
+  let requestType = new MetaTxRequestType(typeHash.toHexString());
+  requestType.typeHash = typeHash;
+  requestType.typeString = event.params.typeStr;
+
+  log.info("creating MetaTxRequestType hash [{}] typeString [{}]", [
+    requestType.typeHash.toHexString(),
+    requestType.typeString,
+  ]);
+  requestType.createdAt = eventUTCMillis(event);
+  requestType.save();
+}
+
+export function handleRequestTypeUnregistered(
+  event: RequestTypeUnregisteredEvent
+): void {
+  let requestTypeId = event.params.typeHash.toHexString();
+  let requestType = MetaTxRequestType.load(requestTypeId);
+  if (requestType == null) {
+    log.error(
+      "received ReqestTypeUnregistered for unknown MetaTxRequestType: {}",
+      [requestTypeId]
+    );
+    return;
+  }
+
+  log.info("removing MetaTxRequestType hash [{}] typeString [{}]", [
+    requestType.typeHash.toHexString(),
+    requestType.typeString,
+  ]);
+  store.remove("MetaTxRequestType", requestTypeId);
+}
+
+export function handleExecutionSuccess(event: ExecutionSuccessEvent): void {
+  let txHash = event.params.txHash;
+  let execution = new MetaTxExecution(txHash.toHexString());
+  execution.txHash = txHash;
+  log.info("creating MetaTxExecution hash [{}]", [txHash.toHexString()]);
+  execution.createdAt = eventUTCMillis(event);
+  execution.save();
+}
+
+/* --------------------------------------------------------------------
+ * Legacy handlers
+ * --------------------------------------------------------------------*/
 
 export function legacyHandleCreateLicensedUserWallet(
   event: LegacyCreateLUWEvent
--- src/srr-mapping.ts
+++ src/srr-mapping.ts
@@ -35,78 +35,77 @@ import {
 import { eventUTCMillis, ZERO_ADDRESS } from './utils'
 
 export function handleTransfer(event: TransferEvent): void {
-  let timestampMillis = eventUTCMillis(event)
-  let srrId = event.params.tokenId.toString()
-  
-  log.info('Transfer for {}', [srrId])
-  log.info('from: {}', [event.params.from.toHexString()])
-  log.info('to: {}', [event.params.to.toHexString()])
-  
-  let srr = SRR.load(srrId)
+  let timestampMillis = eventUTCMillis(event);
+  let srrId = event.params.tokenId.toString();
+
+  log.info("Transfer for {}", [srrId]);
+  log.info("from: {}", [event.params.from.toHexString()]);
+  log.info("to: {}", [event.params.to.toHexString()]);
+
+  let srr = SRR.load(srrId);
   if (srr == null) {
-    srr = new SRR(srrId)
-    srr.tokenId = srrId
-    srr.createdAt = timestampMillis
-    srr.txHash = event.transaction.hash
+    srr = new SRR(srrId);
+    srr.tokenId = srrId;
+    srr.createdAt = timestampMillis;
+    srr.txHash = event.transaction.hash;
   } else if (
-    srr.transferCommitment != null && 
+    srr.transferCommitment != null &&
     event.params.from.toHexString() != ZERO_ADDRESS.toHexString()
   ) {
     // Transfer by commit/reveal
-    log.info('clearing transferCommitment on token = {}', [srr.tokenId])
-    srr.transferCommitment = null
+    log.info("clearing transferCommitment on token = {}", [srr.tokenId]);
+    srr.transferCommitment = null;
   }
 
-  srr.ownerAddress = event.params.to
-  srr.updatedAt = timestampMillis
-  srr.save()
+  srr.ownerAddress = event.params.to;
+  srr.updatedAt = timestampMillis;
+  srr.save();
 
-  let srrCommit = SRRTransferCommit.load(srrId)
+  let srrCommit = SRRTransferCommit.load(srrId);
   if (srrCommit != null) {
-    srrCommit.commitment = null
-    srrCommit.lastAction = 'transfer'
-    srrCommit.updatedAt = timestampMillis
-    srrCommit.save()
+    srrCommit.commitment = null;
+    srrCommit.lastAction = "transfer";
+    srrCommit.updatedAt = timestampMillis;
+    srrCommit.save();
   }
 }
 
 export function handleCreateSRR(event: CreateSRREvent): void {
-  let timestampMillis = eventUTCMillis(event)
-  
-  let srrId = event.params.tokenId.toString()
-  let srr = SRR.load(srrId)
-  
+  let timestampMillis = eventUTCMillis(event);
+
+  let srrId = event.params.tokenId.toString();
+  let srr = SRR.load(srrId);
+
   // SRR should already exist for most tokens because handleTransfer will fire
-  // first. However some tokens created under the old scheme 
+  // first. However some tokens created under the old scheme
   // (RootLogic->StartrailRegistry) will be processed by CreateSRR only.
   // So we handle this here and create the SRR:
   if (srr == null) {
-    srr = new SRR(srrId)
-    srr.tokenId = srrId
-    srr.createdAt = timestampMillis
-    srr.txHash = event.transaction.hash
+    srr = new SRR(srrId);
+    srr.tokenId = srrId;
+    srr.createdAt = timestampMillis;
+    srr.txHash = event.transaction.hash;
   }
 
-  srr.artistAddress = event.params.registryRecord.artistAddress
-  srr.isPrimaryIssuer = event.params.registryRecord.isPrimaryIssuer
-  srr.metadataDigest = event.params.metadataDigest  
+  srr.artistAddress = event.params.registryRecord.artistAddress;
+  srr.isPrimaryIssuer = event.params.registryRecord.isPrimaryIssuer;
+  srr.metadataDigest = event.params.metadataDigest;
 
-  let issuerId = event.params.registryRecord.issuer.toHexString()
-  let luw = LicensedUserWallet.load(issuerId)
+  let issuerId = event.params.registryRecord.issuer.toHexString();
+  let luw = LicensedUserWallet.load(issuerId);
   if (luw != null) {
-    srr.issuer = luw.id
+    srr.issuer = luw.id;
   }
 
-  srr.updatedAt = timestampMillis
-  
-  srr.save()
+  srr.updatedAt = timestampMillis;
 
-  saveSRRMetadataHistory(srr as SRR, event)
+  srr.save();
+
+  saveSRRMetadataHistory(srr as SRR, event);
 }
 
-
 export function handleSRRProvenance(event: SRRProvenanceEvent): void {
-  let params = event.params
+  let params = event.params;
   handleSRRProvenanceInternal(
     event,
     params.tokenId,
@@ -115,12 +114,14 @@ export function handleSRRProvenance(event: SRRProvenanceEvent): void {
     params.timestamp,
     null,
     params.historyMetadataDigest,
-    params.historyMetadataURI,
-  )
+    params.historyMetadataURI
+  );
 }
 
-export function handleSRRProvenanceWithCustomHistory(event: SRRProvenanceWithCustomHistoryEvent): void {
-  let params = event.params
+export function handleSRRProvenanceWithCustomHistory(
+  event: SRRProvenanceWithCustomHistoryEvent
+): void {
+  let params = event.params;
   handleSRRProvenanceInternal(
     event,
     params.tokenId,
@@ -129,14 +130,14 @@ export function handleSRRProvenanceWithCustomHistory(event: SRRProvenanceWithCus
     params.timestamp,
     params.customHistoryId,
     params.historyMetadataDigest,
-    params.historyMetadataURI,
-  )
+    params.historyMetadataURI
+  );
 }
 
 /**
  * Can't use a union type to handle the 2 Provenance events with one function.
- * 
- * So this function exists to handle both with a superset of the 
+ *
+ * So this function exists to handle both with a superset of the
  * available parameters.
  */
 function handleSRRProvenanceInternal(
@@ -149,88 +150,91 @@ function handleSRRProvenanceInternal(
   historyMetadataDigest: string,
   historyMetadataURI: string
 ): void {
-  let srrId = tokenId.toString()
-  let srr = SRR.load(srrId)
+  let srrId = tokenId.toString();
+  let srr = SRR.load(srrId);
   if (srr == null) {
-    log.error('received event for unknown SRR: {}', [srrId])
-    return
+    log.error("received event for unknown SRR: {}", [srrId]);
+    return;
   }
 
   // Update existing SRR
-  srr.ownerAddress = to 
-  srr.updatedAt = eventUTCMillis(event)
-  srr.save()
+  srr.ownerAddress = to;
+  srr.updatedAt = eventUTCMillis(event);
+  srr.save();
 
   // Create new Provenance
-  let provenanceId = crypto.keccak256(
-    ByteArray.fromUTF8(
-      tokenId.toString() +
-      timestamp.toString()
-    )
-  ).toHexString()
-  
-  let provenance = new SRRProvenance(provenanceId)
+  let provenanceId = crypto
+    .keccak256(ByteArray.fromUTF8(tokenId.toString() + timestamp.toString()))
+    .toHexString();
 
-  provenance.srr = srr.id
-  provenance.from = from
-  provenance.to = to
-  
-  provenance.metadataDigest = Bytes.fromHexString(historyMetadataDigest) as Bytes
-  provenance.metadataURI = historyMetadataURI
+  let provenance = new SRRProvenance(provenanceId);
+
+  provenance.srr = srr.id;
+  provenance.from = from;
+  provenance.to = to;
+
+  provenance.metadataDigest = Bytes.fromHexString(
+    historyMetadataDigest
+  ) as Bytes;
+  provenance.metadataURI = historyMetadataURI;
 
   if (customHistoryId) {
     // CustomHistory.load(event.params.customHistoryId)
-    provenance.customHistory = customHistoryId.toString()
-  } 
+    provenance.customHistory = customHistoryId.toString();
+  }
 
-  provenance.timestamp = timestamp
-  provenance.createdAt = eventUTCMillis(event)
-  
-  provenance.save()
+  provenance.timestamp = timestamp;
+  provenance.createdAt = eventUTCMillis(event);
+
+  provenance.save();
 }
 
-export function handleCustomHistoryType(event: CustomHistoryTypeCreatedEvent): void {
-  let id = event.params.id.toString()
+export function handleCustomHistoryType(
+  event: CustomHistoryTypeCreatedEvent
+): void {
+  let id = event.params.id.toString();
 
-  let cht = new CustomHistoryType(id)
-  cht.name = event.params.historyType
-  cht.createdAt = eventUTCMillis(event)
+  let cht = new CustomHistoryType(id);
+  cht.name = event.params.historyType;
+  cht.createdAt = eventUTCMillis(event);
 
-  cht.save()
+  cht.save();
 }
 
 export function handleCustomHistory(event: CustomHistoryCreatedEvent): void {
-  let id = event.params.id.toString()
+  let id = event.params.id.toString();
 
-  let ch = new CustomHistory(id)
-  ch.name = event.params.name 
-  ch.historyType = event.params.customHistoryTypeId.toString()
-  ch.metadataDigest = event.params.metadataDigest
-  ch.createdAt = eventUTCMillis(event)
+  let ch = new CustomHistory(id);
+  ch.name = event.params.name;
+  ch.historyType = event.params.customHistoryTypeId.toString();
+  ch.metadataDigest = event.params.metadataDigest;
+  ch.createdAt = eventUTCMillis(event);
 
-  ch.save()
+  ch.save();
 }
 
 export function handleSRRCommitment(event: SRRCommitmentEvent): void {
-  let params = event.params
+  let params = event.params;
   handleSRRCommitmentInternal(
     event,
     params.owner,
     params.commitment,
     params.tokenId,
     null
-  )
+  );
 }
 
-export function handleSRRCommitmentWithCustomHistory(event: SRRCommitmentWithCustomHistoryEvent): void {
-  let params = event.params
+export function handleSRRCommitmentWithCustomHistory(
+  event: SRRCommitmentWithCustomHistoryEvent
+): void {
+  let params = event.params;
   handleSRRCommitmentInternal(
     event,
     params.owner,
     params.commitment,
     params.tokenId,
     params.customHistoryId
-  )
+  );
 }
 
 function handleSRRCommitmentInternal(
@@ -240,107 +244,117 @@ function handleSRRCommitmentInternal(
   tokenId: BigInt,
   customHistoryId: BigInt
 ): void {
-  let srrId = tokenId.toString()
-  let srr = SRR.load(srrId)
+  let srrId = tokenId.toString();
+  let srr = SRR.load(srrId);
   if (srr == null) {
-    log.error('received event for unknown SRR: {}', [srrId])
-    return
+    log.error("received event for unknown SRR: {}", [srrId]);
+    return;
   }
 
-  log.info('SRRCommitment commitment = {}', [commitment.toHexString()])
+  log.info("SRRCommitment commitment = {}", [commitment.toHexString()]);
 
-  let blockTime = eventUTCMillis(event)
+  let blockTime = eventUTCMillis(event);
 
-  srr.transferCommitment = commitment
-  srr.updatedAt = blockTime
-  srr.save()
+  srr.transferCommitment = commitment;
+  srr.updatedAt = blockTime;
+  srr.save();
 
-  let srrCommit = SRRTransferCommit.load(srrId)
+  let srrCommit = SRRTransferCommit.load(srrId);
   if (srrCommit == null) {
-    srrCommit = new SRRTransferCommit(srrId)
-    srrCommit.createdAt = blockTime
+    srrCommit = new SRRTransferCommit(srrId);
+    srrCommit.createdAt = blockTime;
   }
 
-  srrCommit.commitment = srr.transferCommitment
-  srrCommit.lastAction = 'approve'
+  srrCommit.commitment = srr.transferCommitment;
+  srrCommit.lastAction = "approve";
 
   if (customHistoryId != null) {
-    srrCommit.customHistory = customHistoryId.toString()
+    srrCommit.customHistory = customHistoryId.toString();
   }
 
-  srrCommit.updatedAt = blockTime
-  srrCommit.save()
+  srrCommit.updatedAt = blockTime;
+  srrCommit.save();
 }
 
-export function handleSRRCommitmentCancelled(event: SRRCommitmentCancelledEvent): void {
-  let srrId = event.params.tokenId.toString()
-  let srr = SRR.load(srrId)
+export function handleSRRCommitmentCancelled(
+  event: SRRCommitmentCancelledEvent
+): void {
+  let srrId = event.params.tokenId.toString();
+  let srr = SRR.load(srrId);
   if (srr == null) {
-    log.error('received event for unknown SRR: {}', [srrId])
-    return
+    log.error("received event for unknown SRR: {}", [srrId]);
+    return;
   }
 
-  let blockTime = eventUTCMillis(event)
+  let blockTime = eventUTCMillis(event);
 
-  srr.transferCommitment = null
-  srr.updatedAt = blockTime
-  srr.save()
+  srr.transferCommitment = null;
+  srr.updatedAt = blockTime;
+  srr.save();
 
-  let srrCommit = SRRTransferCommit.load(srrId)
+  let srrCommit = SRRTransferCommit.load(srrId);
   if (srrCommit == null) {
-    log.error(`received event but don't have corresponding SRRTransferCommit: {}`, [srrId])
-    return
+    log.error(
+      `received event but don't have corresponding SRRTransferCommit: {}`,
+      [srrId]
+    );
+    return;
   }
 
-  srrCommit.lastAction = 'cancel'
-  srrCommit.commitment = null
-  srrCommit.updatedAt = blockTime
-  srrCommit.save()
+  srrCommit.lastAction = "cancel";
+  srrCommit.commitment = null;
+  srrCommit.updatedAt = blockTime;
+  srrCommit.save();
 }
 
 export function handleUpdateSRR(event: UpdateSRREvent): void {
-  let srrId = event.params.tokenId.toString()
-  let srr = SRR.load(srrId)
+  let srrId = event.params.tokenId.toString();
+  let srr = SRR.load(srrId);
   if (srr == null) {
-    log.error('received event for unknown SRR: {}', [srrId])
-    return
+    log.error("received event for unknown SRR: {}", [srrId]);
+    return;
   }
 
-  srr.artistAddress = event.params.registryRecord.artistAddress
-  srr.isPrimaryIssuer = event.params.registryRecord.isPrimaryIssuer
-  srr.updatedAt = eventUTCMillis(event)
-  
-  srr.save()
+  srr.artistAddress = event.params.registryRecord.artistAddress;
+  srr.isPrimaryIssuer = event.params.registryRecord.isPrimaryIssuer;
+  srr.updatedAt = eventUTCMillis(event);
+
+  srr.save();
 }
 
-export function handleUpdateSRRMetadataDigest(event: UpdateSRRMetadataDigestEvent): void {
-  let srrId = event.params.tokenId.toString()
-  let srr = SRR.load(srrId)
+export function handleUpdateSRRMetadataDigest(
+  event: UpdateSRRMetadataDigestEvent
+): void {
+  let srrId = event.params.tokenId.toString();
+  let srr = SRR.load(srrId);
   if (srr == null) {
-    log.error('received event for unknown SRR: {}', [srrId])
-    return
+    log.error("received event for unknown SRR: {}", [srrId]);
+    return;
   }
 
-  srr.updatedAt = eventUTCMillis(event)
-  srr.metadataDigest = event.params.metadataDigest
-  srr.save()
+  srr.updatedAt = eventUTCMillis(event);
+  srr.metadataDigest = event.params.metadataDigest;
+  srr.save();
 
-  saveSRRMetadataHistory(srr as SRR, event)
+  saveSRRMetadataHistory(srr as SRR, event);
 }
 
 function saveSRRMetadataHistory(srr: SRR, event: ethereum.Event): void {
-  let metadataHistoryId = crypto.keccak256(
-    ByteArray.fromUTF8(
-      event.transaction.hash.toHexString() +
-      event.logIndex.toHexString() +
-      srr.metadataDigest.toHexString()
+  let metadataHistoryId = crypto
+    .keccak256(
+      ByteArray.fromUTF8(
+        event.transaction.hash.toHexString() +
+          event.logIndex.toHexString() +
+          srr.metadataDigest.toHexString()
+      )
     )
-  ).toHexString()
+    .toHexString();
 
-  let srrMetadataHistory = new SRRMetadataHistory(metadataHistoryId) // metadataHistoryId)
-  srrMetadataHistory.srr = srr.id
-  srrMetadataHistory.createdAt = eventUTCMillis(event)
-  srrMetadataHistory.metadataDigest = Bytes.fromHexString(srr.metadataDigest.toHexString()) as Bytes
-  srrMetadataHistory.save()
+  let srrMetadataHistory = new SRRMetadataHistory(metadataHistoryId); // metadataHistoryId)
+  srrMetadataHistory.srr = srr.id;
+  srrMetadataHistory.createdAt = eventUTCMillis(event);
+  srrMetadataHistory.metadataDigest = Bytes.fromHexString(
+    srr.metadataDigest.toHexString()
+  ) as Bytes;
+  srrMetadataHistory.save();
 }
-
