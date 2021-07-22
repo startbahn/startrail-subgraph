4b782c593f5e275e5fb8824b48d82fb12145dd68
YURIKO NISHIJIMA <yuriko.nishijima@gmail.com>
2021-07-22 21:12:24 +0900

    add test cases for 6 entities

--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -1,7 +1,18 @@
 const Lokka = require("lokka").Lokka
 const Transport = require("lokka-transport-http").Transport
 
-const query = `
+let client: any
+
+beforeAll(() => {
+  client = new Lokka({
+    transport: new Transport(
+      "http://127.0.0.1:8000/subgraphs/name/startbahn/startrail-local"
+    )
+  })
+})
+
+test("srrs", async () => {
+  const query = `
   {
     srrs {
       isPrimaryIssuer
@@ -10,13 +21,6 @@ const query = `
     }
   }
 `
-
-test("data indexed by meta-tx-sr-issue2.json", async () => {
-  const client = new Lokka({
-    transport: new Transport(
-      "http://127.0.0.1:8000/subgraphs/name/startbahn/startrail-local"
-    )
-  })
   const result = await client.query(query)
 
   const data = {
@@ -26,4 +30,333 @@ test("data indexed by meta-tx-sr-issue2.json", async () => {
       "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64"
   }
   expect(result.srrs[0]).toStrictEqual(data)
+})
+
+test("licensedUserWallets ", async () => {
+  const query = `
+  {
+    licensedUserWallets {
+      id
+      walletAddress
+      threshold
+      englishName
+      originalName
+      userType
+      owners
+      salt
+      issuedSRRs {
+        id
+        tokenId
+        isPrimaryIssuer
+        artistAddress
+        issuer {
+          id
+        }
+        ownerAddress
+        metadataDigest
+        transferCommitment
+      }
+      originChain
+      createdAt
+      updatedAt
+    }
+  }
+  `
+  const result = await client.query(query)
+
+  const data = {
+    createdAt: "1626785848000",
+    englishName: "Artist English",
+    id: "0x8b3975ea1fabd1fe60f45a5768a0ee4c4d24c170",
+    issuedSRRs: [
+      {
+        artistAddress: "0x8be37f7589943efb19c0484ca00748dcd6a3cf1a",
+        id: "80626184",
+        isPrimaryIssuer: true,
+        issuer: {
+          id: "0x8b3975ea1fabd1fe60f45a5768a0ee4c4d24c170"
+        },
+        metadataDigest:
+          "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64",
+        ownerAddress: "0x8b3975ea1fabd1fe60f45a5768a0ee4c4d24c170",
+        tokenId: "80626184",
+        transferCommitment: null
+      }
+    ],
+    originChain: null,
+    originalName: "Artist Original",
+    owners: [
+      "0x853f2251666f9d8c45cc760ae10ab0278533d28c",
+      "0x171ea52e619b7fdde870b328ccfb70217a3e32ae",
+      "0xad87f0b51a8788192edd0640ab5ed58e48145c82"
+    ],
+    salt: "0x64e604787cbf194841e7b68d7cd28786f6c9a0a3ab9f8b0a0e87cb4387ab0001",
+    threshold: 1,
+    updatedAt: "1626785848000",
+    userType: "artist",
+    walletAddress: "0x8b3975ea1fabd1fe60f45a5768a0ee4c4d24c170"
+  }
+  expect(result.licensedUserWallets[0]).toStrictEqual(data)
+})
+
+test("customHistoryType", async () => {
+  const query = `
+  {
+    customHistoryTypes {
+      id
+      name
+      createdAt
+    }
+  }
+`
+  const result = await client.query(query)
+
+  const data = [
+    {
+      createdAt: "1626785819000",
+      id: "1",
+      name: "auction"
+    },
+    {
+      createdAt: "1626785820000",
+      id: "2",
+      name: "exhibition"
+    }
+  ]
+  expect(result.customHistoryTypes).toStrictEqual(data)
+})
+
+test("srrmetadataHistories", async () => {
+  const query = `
+  {
+    srrmetadataHistories {
+      id
+      srr {
+        id
+        tokenId
+        isPrimaryIssuer
+        artistAddress
+        ownerAddress
+        metadataDigest
+        transferCommitment
+        metadataHistory {
+          id
+          srr {
+            id
+            isPrimaryIssuer
+            artistAddress
+            metadataDigest
+          }
+          metadataDigest
+          createdAt
+        }
+        originChain
+        originTxHash
+        createdAt
+        updatedAt
+      }
+      metadataDigest
+      createdAt
+    }
+  }
+`
+  const result = await client.query(query)
+
+  const data = {
+    createdAt: "1626785849000",
+    id: "0xa9d166e3c5bf32432832205fef068c3b797fd91ae75600d61db63467b42fd7ac",
+    metadataDigest:
+      "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64",
+    srr: {
+      artistAddress: "0x8be37f7589943efb19c0484ca00748dcd6a3cf1a",
+      createdAt: "1626785849000",
+      id: "80626184",
+      isPrimaryIssuer: true,
+      metadataDigest:
+        "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64",
+      metadataHistory: [
+        {
+          createdAt: "1626785849000",
+          id:
+            "0xa9d166e3c5bf32432832205fef068c3b797fd91ae75600d61db63467b42fd7ac",
+          metadataDigest:
+            "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64",
+          srr: {
+            artistAddress: "0x8be37f7589943efb19c0484ca00748dcd6a3cf1a",
+            id: "80626184",
+            isPrimaryIssuer: true,
+            metadataDigest:
+              "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64"
+          }
+        }
+      ],
+      originChain: "eip155:31337",
+      originTxHash:
+        "0x9a6cd7a5c3495b37613418d7c62ff01637e683a7077a19a7c3d34633b044e189",
+      ownerAddress: "0x8b3975ea1fabd1fe60f45a5768a0ee4c4d24c170",
+      tokenId: "80626184",
+      transferCommitment: null,
+      updatedAt: "1626785849000"
+    }
+  }
+  expect(result.srrmetadataHistories[0]).toStrictEqual(data)
+})
+
+test("metaTxRequestTypes", async () => {
+  const query = `
+  {
+    metaTxRequestTypes {
+      id
+      typeHash
+      typeString
+      createdAt
+    }
+  }
+`
+  const result = await client.query(query)
+
+  const data = [
+    {
+      createdAt: "1626785832000",
+      id: "0x052d8d1acdbf73c1b466436c3bc062709a28af704363f8b326314d7ab01ce47b",
+      typeHash:
+        "0x052d8d1acdbf73c1b466436c3bc062709a28af704363f8b326314d7ab01ce47b",
+      typeString:
+        "BulkTransferSendBatch(address from,uint256 nonce,bytes32 merkleRoot)"
+    },
+    {
+      createdAt: "1626785838000",
+      id: "0x1f6bcc34496ca1b52d584f9a76e6a39b27989a9c186f8bcac08b53d1b03bb293",
+      typeHash:
+        "0x1f6bcc34496ca1b52d584f9a76e6a39b27989a9c186f8bcac08b53d1b03bb293",
+      typeString:
+        "StartrailRegistryAddHistory(address from,uint256 nonce,bytes data,uint256[] tokenIds,uint256[] customHistoryIds)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0x425002ddfe8a5210fcf678ff38ca336c9b3babbf2f10efa5fdeaace5951e2b48",
+      typeHash:
+        "0x425002ddfe8a5210fcf678ff38ca336c9b3babbf2f10efa5fdeaace5951e2b48",
+      typeString:
+        "WalletSetOriginalName(address from,uint256 nonce,bytes data,address wallet,string name)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0x72aa5b313beffb167251bd88ac4c617700ece940a29fc55df7dea54d0e47f5c6",
+      typeHash:
+        "0x72aa5b313beffb167251bd88ac4c617700ece940a29fc55df7dea54d0e47f5c6",
+      typeString:
+        "WalletSetEnglishName(address from,uint256 nonce,bytes data,address wallet,string name)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0x9eb400930ea9ba11485a1fbe22ec6b4f1f4b74ec9c1f867ed95185ef2f67092c",
+      typeHash:
+        "0x9eb400930ea9ba11485a1fbe22ec6b4f1f4b74ec9c1f867ed95185ef2f67092c",
+      typeString:
+        "WalletRemoveOwner(address from,uint256 nonce,address wallet,address prevOwner,address owner,uint256 threshold)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xa2c5a6fe8768fd09231270ba9fee50bd8355a89a393cedf6d8171fe3a7fa075c",
+      typeHash:
+        "0xa2c5a6fe8768fd09231270ba9fee50bd8355a89a393cedf6d8171fe3a7fa075c",
+      typeString:
+        "BulkIssueSendBatch(address from,uint256 nonce,bytes32 merkleRoot)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xa32f0c35f201b6260c799d8f4f0c66d8d05a3e60db8cdeb795ab26987366a155",
+      typeHash:
+        "0xa32f0c35f201b6260c799d8f4f0c66d8d05a3e60db8cdeb795ab26987366a155",
+      typeString:
+        "WalletSwapOwner(address from,uint256 nonce,address wallet,address prevOwner,address oldOwner,address newOwner)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xa51d034042007707c0d3a28048543b905e0c5b8646875ced2aa5f128895ee1fb",
+      typeHash:
+        "0xa51d034042007707c0d3a28048543b905e0c5b8646875ced2aa5f128895ee1fb",
+      typeString:
+        "StartrailRegistryCreateSRR(address from,uint256 nonce,bool isPrimaryIssuer,address artistAddress,bytes32 metadataDigest)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xb4d22bf06a762d0a27f1c25c8a258e0982dbc92abe4d6d72fdbec60d49464daa",
+      typeHash:
+        "0xb4d22bf06a762d0a27f1c25c8a258e0982dbc92abe4d6d72fdbec60d49464daa",
+      typeString:
+        "StartrailRegistryApproveSRRByCommitment(address from,uint256 nonce,bytes data,uint256 tokenId,bytes32 commitment,string historyMetadataDigest)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xd111846f191cf3c32ce6aedccf704a641a50381f22729d5a6cd5f13a0554ef80",
+      typeHash:
+        "0xd111846f191cf3c32ce6aedccf704a641a50381f22729d5a6cd5f13a0554ef80",
+      typeString:
+        "StartrailRegistryUpdateSRRMetadata(address from,uint256 nonce,uint256 tokenId,bytes32 metadataDigest)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xe40b02b26d5443f4479036538f991a995624f5cc199ecab72a0dde126115a16c",
+      typeHash:
+        "0xe40b02b26d5443f4479036538f991a995624f5cc199ecab72a0dde126115a16c",
+      typeString:
+        "WalletAddOwner(address from,uint256 nonce,address wallet,address owner,uint256 threshold)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xebdf6224c1bb9fe61ea31b37d9b143c5b2c3b390aa1970cd68f6e4cdcb087e06",
+      typeHash:
+        "0xebdf6224c1bb9fe61ea31b37d9b143c5b2c3b390aa1970cd68f6e4cdcb087e06",
+      typeString:
+        "StartrailRegistryApproveSRRByCommitmentWithCustomHistoryId(address from,uint256 nonce,bytes data,uint256 tokenId,bytes32 commitment,string historyMetadataDigest,uint256 customHistoryId)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xf6f490ad3237fb8a6611e8cb20e7dc248e4df8fb091d3ebebfb536dc98e54397",
+      typeHash:
+        "0xf6f490ad3237fb8a6611e8cb20e7dc248e4df8fb091d3ebebfb536dc98e54397",
+      typeString:
+        "StartrailRegistryUpdateSRR(address from,uint256 nonce,uint256 tokenId,bool isPrimaryIssuer,address artistAddress)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xfc61e554d8da03571436be8820948b7c044eb5abfddb2394091c1fdebc71b5fd",
+      typeHash:
+        "0xfc61e554d8da03571436be8820948b7c044eb5abfddb2394091c1fdebc71b5fd",
+      typeString:
+        "StartrailRegistryCancelSRRCommitment(address from,uint256 nonce,uint256 tokenId)"
+    },
+    {
+      createdAt: "1626785824000",
+      id: "0xff1b6ae7ba3b6fcaf8441b4b7ebbebb22444db035e8eb25feb17296a6c00b54b",
+      typeHash:
+        "0xff1b6ae7ba3b6fcaf8441b4b7ebbebb22444db035e8eb25feb17296a6c00b54b",
+      typeString:
+        "WalletChangeThreshold(address from,uint256 nonce,address wallet,uint256 threshold)"
+    }
+  ]
+  expect(result.metaTxRequestTypes).toStrictEqual(data)
+})
+
+test("metaTxExecutions", async () => {
+  const query = `
+  {
+    metaTxExecutions {
+      id
+      txHash
+      createdAt
+    }
+  }
+`
+  const result = await client.query(query)
+
+  const data = {
+    createdAt: "1626785849000",
+    id: "0x7581cb842e565b3e27a43a7ff9ff28f8876c50938e93ef45c9ac972d7ed9be30",
+    txHash: "0x7581cb842e565b3e27a43a7ff9ff28f8876c50938e93ef45c9ac972d7ed9be30"
+  }
+
+  expect(result.metaTxExecutions[0]).toStrictEqual(data)
 })
